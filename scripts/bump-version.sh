b3c20a7f7ec75523e1c4af410ef35ffe05de5c4f
Mayuresh Kadu <23300+mskadu@users.noreply.github.com>
2026-06-02 22:20:09 +0100

    fix: bump-version.sh now updates README version badge automatically

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -14,4 +14,9 @@ new_minor=$((minor + 1))
 new_version="$major.$new_minor.0"
 
 echo "$new_version" > "$VERSION_FILE"
+
+# Update README badge
+README_FILE="$ROOT/README.md"
+sed -i '' "s/Current version: [0-9]*\.[0-9]*\.[0-9]*/Current version: $new_version/" "$README_FILE"
+
 echo "$current -> $new_version"
